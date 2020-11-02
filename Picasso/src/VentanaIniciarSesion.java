fbc418244db46f0d6ad864c8f97a47bb90ba8a76
Iker <ikermorales@opendeusto.es>
2020-11-02 09:28:10 +0100

    Base de datos implementada.

--- Picasso/src/VentanaIniciarSesion.java
+++ Picasso/src/VentanaIniciarSesion.java
@@ -1,10 +1,15 @@
-import java.awt.*; 
-import java.awt.event.ActionEvent;
+import java.awt.*;  
+import java.awt.event.*;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TimerTask;
@@ -13,6 +18,8 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 public class VentanaIniciarSesion extends JFrame {
 
@@ -32,14 +39,14 @@ public class VentanaIniciarSesion extends JFrame {
 
 	private JPanel panelDecorativo;
 	private JLabel labelDecorativo;
-	
+
 	private File file = new File("usuariosdata.txt");
-	
+
 	private static ComponentePapel componente;
 	private static VentanaMenu menu;
 	private static Papel papel;
 	private static String usuarioEscogido;
-	
+
 	public VentanaIniciarSesion() {
 		setTitle("Inicio");
 		setSize(310,260);
@@ -84,10 +91,13 @@ public class VentanaIniciarSesion extends JFrame {
 		panelBotonera = new JPanel();
 		aceptar = new JButton("Iniciar Sesión");
 		crearUsuario = new JButton("Crear Nuevo Usuario");
+		aceptar.setEnabled(false);
+		crearUsuario.setEnabled(false);
 		panelBotonera.add(aceptar);
 		panelBotonera.add(crearUsuario);
 		panelBotonera.setBackground(new Color(188, 245, 175));
 		panelDatos.add(panelBotonera);
+
 
 		aceptar.addActionListener(new ActionListener() {
 
@@ -101,85 +111,156 @@ public class VentanaIniciarSesion extends JFrame {
 					JButton botonInicio = new JButton("Iniciar Picasso");
 					panelDatos.add(botonInicio);
 					botonInicio.addActionListener(new ActionListener() {
-						
+
 						@Override
 						public void actionPerformed(ActionEvent e) {
 							papel = new Papel();
 							componente = new ComponentePapel(papel);
 							menu = new VentanaMenu(componente, papel, usuarioEscogido);
 							dispose();
-							
+
 						}
 					});
 					repaint();
 					validate();					
-					
+
 				} else {
 					JOptionPane.showMessageDialog(null, "Usuario y/o contraseña incorrectas");
 				}
 			}
 		});
 
+
 		crearUsuario.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				crearUsuario();
-				
-
 			}
 		});
 
+
+
+		usuario.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				changed();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				changed();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				changed();
+			}
+		});
+
+		contraseña.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				changed();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				changed();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				changed();
+			}
+		});		
 
 		setVisible(true);		
 	}
 
 
+
+
+
+
+	public void changed() {
+		if (usuario.getText().equals("") || contraseña.getText().equals("")
+			|| usuario.getText().contains(" ") || contraseña.getText().contains(" ")
+			|| usuario.getText().contains(";") || contraseña.getText().contains(";")
+			|| usuario.getText().contains("(") || contraseña.getText().contains("(")
+			|| usuario.getText().contains(")") || contraseña.getText().contains(")")) {
+			aceptar.setEnabled(false);
+			crearUsuario.setEnabled(false);
+		}
+		else {
+			aceptar.setEnabled(true);
+			crearUsuario.setEnabled(true);
+		}
+	}
+
+
+
 	public boolean comprobar() {
 		try {
-			Scanner sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				String linea = sc.nextLine();
-				String[] campos = linea.split(";");
-				for (int i = 0; i < campos.length; i++) {
-					if (i % 2 == 0 && campos[i].equals(usuario.getText()) && campos[i+1].equals(contraseña.getText())) { 
-						usuarioEscogido = campos[i];
-						return true;
-					}
+			Class.forName("org.sqlite.JDBC");
+
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:usuarios.db");
+			Statement stmt = (Statement) conn.createStatement();
+			ResultSet rs = stmt.executeQuery("Select * from usuarios");
+
+			while(rs.next()) {
+				String usuarioBD = rs.getString("usuario");
+				String contraseñaBD = rs.getString("contraseña");
+				System.out.println(usuarioBD);
+				System.out.println(contraseñaBD);
+				if (usuarioBD.equals(usuario.getText()) && contraseñaBD.equals(contraseña.getText())) {
+					usuarioEscogido = usuarioBD;
+					return true;
 				}
+
 			}
 
-		} catch (FileNotFoundException e1) {
-			JOptionPane.showMessageDialog(null, "No se encontró el archivo: usuariosdata.txt");
-		}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 		return false;
 	}
 
 
-	
+
 	public void crearUsuario() {
-		if (!usuario.getText().isEmpty() && !contraseña.getText().isEmpty()) {
-			try {
-				File directorioPersonal = new File(usuario.getText() + "\\galeria\\");
-				if (!directorioPersonal.exists()){
-					FileWriter fw = new FileWriter(file, true);
-					fw.append(usuario.getText() +  ";" + contraseña.getText() + ";");
-					fw.flush();
-					fw.close();		
-					directorioPersonal.mkdirs();
-					JOptionPane.showMessageDialog(null, "Usuario creado con éxito");
 
-				} else {
-					JOptionPane.showMessageDialog(null, "Este usuario ya existe");
+		try {
+			Class.forName("org.sqlite.JDBC");
 
-				}
-			} catch (IOException e2) {
-				JOptionPane.showMessageDialog(null, "No se pudo leer el archivo: " + file);
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:usuarios.db");
+			Statement stmt = (Statement) conn.createStatement();
+
+			ResultSet rs1 = stmt.executeQuery("Select * from usuarios");
+
+			ArrayList<String> usuariosBD = new ArrayList<>();
+
+			while(rs1.next()) {
+				String usuarioBD = rs1.getString("usuario");
+				String contraseñaBD = rs1.getString("contraseña");
+				usuariosBD.add(usuarioBD);
 			}
-		} else {
-			JOptionPane.showMessageDialog(null, "Para crear un usuario rellena los campos 'Usuario y 'Contraseña'.");
-		}
-	}
-	
+			rs1.close();
+
+			if (!usuariosBD.contains(usuario.getText())) {
+				String instruccion = "INSERT INTO usuarios (usuario, contraseña) VALUES ('" + usuario.getText()  + "','" + contraseña.getText() + "');";
+				File directorioPersonal = new File("clientes\\" + usuario.getText() + "\\galeria\\");
+				directorioPersonal.mkdirs();
+				JOptionPane.showMessageDialog(null, "Usuario creado con éxito");
+				int rs2 = stmt.executeUpdate(instruccion);
+				
+			} else {
+				JOptionPane.showMessageDialog(null, "Este usuario ya existe");
+			}
+			
+			stmt.close();
+			conn.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Se ha ejecutado el metodo de crear usuario a pesar del error");
+		}	
+
+	} 
+
+
+
 }
