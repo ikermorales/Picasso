d1cb1b99858f5e3fa87b94df99ab46347b609468
David Herrero <d.herrero@opendeusto.es>
2020-11-08 15:54:42 +0100

    VentanaProceso arreglada

--- Picasso/src/VentanaProceso.java
+++ Picasso/src/VentanaProceso.java
@@ -1,7 +1,8 @@
-import java.awt.*;
+import java.awt.*; 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -9,7 +10,6 @@ import javax.swing.border.TitledBorder;
 
 
 public class VentanaProceso extends JFrame{
-	
 	private JPanel panelProceso;
 	private JLabel imagenProceso = new JLabel();
 	private JPanel panelSeleccion;
@@ -20,7 +20,7 @@ public class VentanaProceso extends JFrame{
 	private HashMap<Integer, String> hashPaginas = new HashMap<>();
 
 
-	public VentanaProceso(ComponentePapel cp, Papel p) {
+	public VentanaProceso(ComponentePapel cp, Papel p) {  
 		setTitle("Proceso");
 		setSize(240, 385);
 		setLocationRelativeTo(null);
@@ -32,75 +32,82 @@ public class VentanaProceso extends JFrame{
 		add(panelProceso);
 
 		panelSeleccion = new JPanel();
-		panelSeleccion.setPreferredSize(new Dimension(400,37));
+		panelSeleccion.setPreferredSize(new Dimension(400, 37));
 		panelSeleccion.setBorder(new LineBorder(Color.lightGray));
 		add(panelSeleccion);
-
 
 		for (int i = 0; i < cp.getContadorImagen(); i++) { 
 			hashPaginas.put(i, "proceso/" + i + ".jpg");
 		}
+	
 
-		
 		avanzar = new JButton(">");
 		avanzar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(pagina < cp.getContadorImagen() - 1) {
-
+				if(pagina < cp.getContadorImagen() - 1) { 
 					pagina++;
 					ImageIcon ic = new ImageIcon(hashPaginas.get(pagina));
 					Image imagen = ic.getImage(); 
 					Image nuevaimagenreeslada = imagen.getScaledInstance(200, 300,  java.awt.Image.SCALE_SMOOTH);
 					ic = new ImageIcon(nuevaimagenreeslada);  
 					imagenProceso.setIcon(ic);
-					separador.setLabel(pagina + " / " + (cp.getContadorImagen() - 1) );
+					separador.setLabel(pagina + " / " + (cp.getContadorImagen() - 1) ); 
 					repaint();
 					validate();
-
+					System.out.println(pagina);
 				}
+
 			}
 		});
-
-
 		retroceder = new JButton("<");
 		retroceder.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (pagina >= 1) {	//Este esta bien
+				if (pagina >= 1) {	//Este ta bien
 					pagina--;
 					ImageIcon ic = new ImageIcon(hashPaginas.get(pagina));
 					Image imagen = ic.getImage(); 
-					Image nuevaimagenreeslada = imagen.getScaledInstance(200, 300,  java.awt.Image.SCALE_SMOOTH);					ic = new ImageIcon(nuevaimagenreeslada);  
+					Image nuevaimagenreeslada = imagen.getScaledInstance(200, 300,  java.awt.Image.SCALE_SMOOTH);
+					ic = new ImageIcon(nuevaimagenreeslada);  
 					imagenProceso.setIcon(ic);
-					separador.setLabel(pagina + " / " + (cp.getContadorImagen() - 1) );
+					separador.setLabel(pagina + " / " + (cp.getContadorImagen() - 1) ); 
 					repaint();
 					validate();
-
+					System.out.println(pagina);
 				}
+
 			}
 		});
-
 
 		separador = new JButton(pagina + " /" + (cp.getContadorImagen() - 1) ); 
 		panelSeleccion.add(retroceder);
 		panelSeleccion.add(separador);
 		panelSeleccion.add(avanzar);
 		panelProceso.add(imagenProceso);
-
 		separador.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cp.cargarImagenConcreta(pagina,p);
-				cp.repaint();
-				p.repaint();
+				
+				cp.setDibujosGrandes(new ArrayList<>());
+				cp.setDibujos(new ArrayList<>());
+				cp.forRepaint();
+				cp.setDibujosGrandes(cp.getHashDibujos().get(pagina));
+				cp.forRepaint();
+				
+				cp.setContadorImagen(pagina + 1);
 				dispose();
+				p.toFront();
+				p.repaint();
+				cp.forRepaint();
+				cp.generarEstado(p);
 			}
 		});
-
+		
+	
 		//INIT
 		ImageIcon ic = new ImageIcon(hashPaginas.get(pagina));
 		Image imagen = ic.getImage(); 
