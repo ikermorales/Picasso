99143c596d51c042b4ad6b2df1250ddb71e3b7a1
Gorka de Miguel <gorka.demiguel@opendeusto.es>
2020-11-08 16:15:11 +0100

    ventana decorada y funcionan algunas cosas que antes no funcionaban

--- Picasso/src/VentanaMenu.java
+++ Picasso/src/VentanaMenu.java
@@ -1,15 +1,21 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.border.TitledBorder;
@@ -17,22 +23,22 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class VentanaMenu extends JFrame {
-
+	
 	private JPanel panelPinceles;
 	private JComboBox<String> pincelesCombo;
-
+	private JLabel pincelPng;
+	
 	private JButton botonPaleta;
 	private Paleta paleta;
-
+	
 	private JButton botonProceso;
 	private VentanaProceso proceso;
 
 	private JButton botonArcoiris;
-
+	
 	private JButton botonBorrarTodo;
 
 	private JButton botonTexto;
-
 	private VentanaTexto ventanaTexto;
 	
 	private JButton botonGuardar;
@@ -40,83 +46,101 @@ public class VentanaMenu extends JFrame {
 	
 	private JPanel panelTamanyo;
 	private JSlider sliderTamanyo;
+	private JLabel tamanyoPng;
 	
+	private JPanel panelPuedePintar;
+	private JCheckBox checkPuedePintar;
+	private Thread hiloCheck;
 
+	
 	public VentanaMenu(ComponentePapel cp, Papel p, String usuario) {
 		setTitle("Menu");
 		setSize(new Dimension(310, 400));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new GridLayout(5,1)); 
-
+		
 		panelPinceles = new JPanel();
+		panelPinceles.setLayout(new GridBagLayout());
+		pincelPng = new JLabel();
+		pincelPng.setIcon(new ImageIcon("iconos/caligrafico.png"));
 		panelPinceles.setBorder(new TitledBorder("Pinceles: "));
 		pincelesCombo = new JComboBox<>();
-		pincelesCombo.addItem("Bolígrafo");
-		pincelesCombo.addItem("Pixelado");
+		pincelesCombo.addItem("Caligrafico");
 		pincelesCombo.addItem("Suave");
-		pincelesCombo.addItem("Triangular");
-		pincelesCombo.addItem("Corazón");
+		pincelesCombo.addItem("Pixelado");
+		
+		int sizePincelesCombo = pincelesCombo.getItemCount();
+		for (int i = 0; i < sizePincelesCombo; i++) {
+		  String item = pincelesCombo.getItemAt(i);
+		  cp.getPinceles().add(item);
+		}
+		
 		pincelesCombo.addActionListener(new ActionListener() {
-
+			 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				cp.setPincel(pincelesCombo.getSelectedIndex());
-
+				cp.setSelectorSprite(pincelesCombo.getSelectedIndex());
+				pincelPng.setIcon(new ImageIcon("iconos/" + pincelesCombo.getSelectedItem() + ".png"));
 			}
 		});
 		panelPinceles.add(pincelesCombo);
+		panelPinceles.add(pincelPng);
 		add(panelPinceles);
-		botonPaleta = new JButton("Paleta");
+		
+		
+		botonPaleta = new JButton("Paleta", new ImageIcon("iconos/paleta.png"));
 		botonPaleta.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				paleta = new Paleta(cp);
 			}
 		});
 		add(botonPaleta);
-
-		botonProceso = new JButton("Proceso");
+		
+		
+		botonProceso = new JButton("Historial", new ImageIcon("iconos/historial.png"));
 		botonProceso.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				proceso = new VentanaProceso(cp, p);
-
-
+				
+				
 			}
 		});
 		add(botonProceso);
 		
-		botonArcoiris = new JButton("Arcoíris");
+
+		botonArcoiris = new JButton("Arcoíris", new ImageIcon("iconos/arcoiris.png"));
 		botonArcoiris.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
 				if(!cp.isRainbowActivado()) {
 					cp.setRainbowActivado(true);
 					cp.dibujarRainbow();
 					cp.getHiloArcoiris().start();
-				}	
+				}
 			}
 		});
-		
 		add(botonArcoiris);
 		
 		
-		botonBorrarTodo = new JButton("Nuevo dibujo");
+		botonBorrarTodo = new JButton("Nuevo dibujo", new ImageIcon("iconos/papel.png"));
 		botonBorrarTodo.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cp.borrarTodo(p);
-				
+				cp.borrarTodo(p);	
+				cp.setDibujosGrandes(new ArrayList<>());
 			}
 		});
 		add(botonBorrarTodo);
 		
 		
-		botonTexto = new JButton("Texto");
+		botonTexto = new JButton("Texto", new ImageIcon("iconos/texto.png"));
 		botonTexto.addActionListener(new ActionListener() {
 			
 			@Override
@@ -128,7 +152,7 @@ public class VentanaMenu extends JFrame {
 		add(botonTexto);
 		
 		
-		botonGuardar = new JButton("Guardar");
+		botonGuardar = new JButton("Guardar", new ImageIcon("iconos/guardar.png"));
 		botonGuardar.addActionListener(new ActionListener() {
 			
 			@Override
@@ -138,11 +162,10 @@ public class VentanaMenu extends JFrame {
 			}
 		});
 		add(botonGuardar);
-
-
+		
+		
 		sliderTamanyo = new JSlider(); 
-		sliderTamanyo.setPreferredSize(new Dimension(120, 40));
-		sliderTamanyo.setSize(getMinimumSize());
+		sliderTamanyo.setPreferredSize(new Dimension(100, 40));
 		sliderTamanyo.setBorder(new TitledBorder("Tamaño: " + cp.getTamanyo()));
 		sliderTamanyo.setValue(7);
 		sliderTamanyo.setPaintTicks(false);
@@ -154,16 +177,33 @@ public class VentanaMenu extends JFrame {
 			public void stateChanged(ChangeEvent e) {
 				cp.setTamanyo(sliderTamanyo.getValue());
 				sliderTamanyo.setBorder(new TitledBorder("Tamaño: " + sliderTamanyo.getValue()));
+		
 			}
 		});
 		panelTamanyo = new JPanel();
+		tamanyoPng = new JLabel();
+		tamanyoPng.setIcon(new ImageIcon("iconos/size.png"));
 		panelTamanyo.getMinimumSize();
 		panelTamanyo.add(new JLabel(new ImageIcon()));
 		panelTamanyo.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0)); //TOC
 		panelTamanyo.add(sliderTamanyo);
+		panelTamanyo.add(tamanyoPng);
 		add(panelTamanyo);
+		
+
 		
 		setVisible(true);
 	}
 
+
+	public JCheckBox getCheckPuedePintar() {
+		return checkPuedePintar;
+	}
+	public void setCheckPuedePintar(JCheckBox checkPuedePintar) {
+		this.checkPuedePintar = checkPuedePintar;
+	}
+	
+	
+	
+	
 }
