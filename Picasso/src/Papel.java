7a2e285b8fc315c6ae3e67135b09d85d340434ee
Gorka de Miguel <gorka.demiguel@opendeusto.es>
2020-11-08 16:12:51 +0100

    papel optimizado

--- Picasso/src/Papel.java
+++ Picasso/src/Papel.java
@@ -1,25 +1,33 @@
-import java.awt.GridLayout;
+import java.awt.GridLayout;  
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Papel extends JFrame {
 
-	private ComponentePapel compPapel;
-	private int anchura = 600;//esto tiene que poder cambiarse
+	private static ComponentePapel compPapel;
+	private int anchura = 600;
 	private int altura = 800;
+
 
 	public Papel() {
 		setTitle("Picasso");
 		setSize(anchura, altura);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new GridLayout(1,1));
 		setResizable(false);
+		setLocationRelativeTo(null);
 
 		compPapel = new ComponentePapel(this);
 		add(compPapel);
-
+		
+		
 		setVisible(true);
-	}	
-
+	}
+	
 	
 	public int getAnchura() {
 		return anchura;
@@ -34,13 +42,11 @@ public class Papel extends JFrame {
 	public void setAltura(int altura) {
 		this.altura = altura;
 	}
-	
-	
-
-
-	
-	
-
-	
+	public ComponentePapel getCompPapel() {
+		return compPapel;
+	}
+	public void setCompPapel(ComponentePapel compPapel) {
+		this.compPapel = compPapel;
+	}
 	
 } 
