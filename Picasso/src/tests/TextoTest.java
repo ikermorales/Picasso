ff714285e763d0f0f9842e0a1b9e5faf1b1f63e9
David Herrero <d.herrero@opendeusto.es>
2020-11-22 16:42:57 +0100

    TextoTest actualizado

--- Picasso/src/tests/TextoTest.java
+++ Picasso/src/tests/TextoTest.java
@@ -10,12 +10,12 @@ public class TextoTest {
 
 	@Test
 	public void testGetContenido() {
-		Texto txt = new Texto(0, 0, 0, 0, null, 0, null, 0, 0, false, false, "Boton");
+		Texto txt = new Texto(0, 0, 0, 0, null, 0, 0, null, 0, 0, false, false, "Boton");
 		assertEquals("boton", txt.getContenido());
 	}
 	@Test
 	public void testSetContenido() {
-		Texto txt = new Texto(0, 0, 0, 0, null, 0, null, 0, 0, false, false, "Boton");
+		Texto txt = new Texto(0, 0, 0, 0, null, 0, 0, null, 0, 0, false, false, "Boton");
 		txt.setContenido("Patata");
 		assertEquals("Patata", txt.getContenido());
 	}
