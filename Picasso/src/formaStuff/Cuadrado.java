b4e7daff159951ee5459a46b7354e8c371bc63be
David Herrero <d.herrero@opendeusto.es>
2020-12-22 21:37:58 +0100

    Clase Cuadrado

---
+++ Picasso/src/formaStuff/Cuadrado.java
@@ -0,0 +1,52 @@
+package formaStuff;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+import java.util.ArrayList;
+
+public class Cuadrado extends Forma {
+	private double alto;
+	private double ancho;
+	
+	
+	public Cuadrado(int x, int y, int xV, int yV, Color color, int opacidad, int tamanyo, ArrayList<String> sprites,
+			int sprite, double collisionRad, boolean simetriaActivada, boolean simetriaHorizontal,
+			boolean estanRellenas, Integer botonteclado, double alto, double ancho) {
+		super(x, y, xV, yV, color, opacidad, tamanyo, sprites, sprite, collisionRad, simetriaActivada,
+				simetriaHorizontal, estanRellenas, botonteclado);
+		this.alto = alto;
+		this.ancho = ancho;
+	}
+	
+	
+
+	public Cuadrado() {
+		super();
+		this.alto = 0;
+		this.ancho = 0;
+	}
+	
+	
+	public double getAlto() {
+		return alto;
+	}
+	public void setAlto(double alto) {
+		this.alto = alto;
+	}
+	public double getAncho() {
+		return ancho;
+	}
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+	@Override
+	public String toString() {
+		return "Cuadrado [alto=" + alto + ", ancho=" + ancho + ", getColor()=" + getColor() + ", isEstanRellenas()="
+				+ isEstanRellenas() + ", toString()=" + super.toString() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + "]";
+	}
+	
+	
+	
+}
