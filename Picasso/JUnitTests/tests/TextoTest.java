af20c15c0eb2370b8ee72b370b27ffeed343a340
David Herrero <d.herrero@opendeusto.es>
2021-01-10 16:58:52 +0100

    coverage TextoTest aumentado

---
+++ Picasso/JUnitTests/tests/TextoTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import clasesBase.Texto;
+
+public class TextoTest {
+
+	@Test
+	public void testGetContenido() {
+		Texto txt = new Texto(0, 0, 0, 0, null, 0, 0, null, 0, 0, false, false, "Boton");
+		assertEquals("Boton", txt.getContenido());
+	}
+	@Test
+	public void testSetContenido() {
+		Texto txt = new Texto(0, 0, 0, 0, null, 0, 0, null, 0, 0, false, false, "Boton");
+		txt.setContenido("Patata");
+		assertEquals("Patata", txt.getContenido());
+	}
+
+}
